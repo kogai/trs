8286513834e8d37c79407cdbec501bc011f4bf89
小飼慎一 <kogai0121@gmail.com>
2017-05-03 22:18:01 +0900

    Parse response as untagged enum

--- src/translate.rs
+++ src/translate.rs
@@ -3,42 +3,62 @@ use reqwest::Client;
 use serde_json;
 
 const API_TRANSLATE: &'static str = "https://www.googleapis.com/language/translate/v2";
-// const API_DETECT: &'static str = "https://www.googleapis.com/language/translate/v2/detect";
+const API_DETECT: &'static str = "/detect";
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct ErrorReason {
     domain: String,
     reason: String,
     message: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Errors {
     errors: Vec<ErrorReason>,
     code: i32,
     message: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct TranslateResponseError {
-    error: Errors,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
 struct Translations {
     translated_text: String,
     detected_source_language: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Data {
     translations: Vec<Translations>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct TranslateResponseSuccess {
-    data: Data,
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
+enum Translate {
+    Success { data: Data },
+    Error { error: Errors },
+}
+
+#[test]
+fn it_should_parse_success() {
+    assert_eq!(serde_json::from_str::<Translate>(r#"
+    {
+        "data": {
+            "translations": [
+                {
+                    "translatedText": "hi",
+                    "detectedSourceLanguage": "en"
+                }
+            ]
+        }
+    }
+    "#).unwrap(), Translate::Success {
+        data: Data {
+            translations: vec![Translations {
+                translated_text: "hi".to_owned(),
+                detected_source_language: "en".to_owned(),
+            }]
+        }
+    });
 }
 
 pub fn translate(api_key: String, target_language: String, query_text: String) -> String {
@@ -55,24 +75,21 @@ pub fn translate(api_key: String, target_language: String, query_text: String) -
         .expect("send Request failed")
         .read_to_string(&mut buffer)
         .expect("read response failed");
-
-    let response = serde_json::from_str::<TranslateResponseSuccess>(&buffer);
+    
+    let response = serde_json::from_str::<Translate>(&buffer).unwrap();
     match response {
-        Ok(r) => {
-            r.data
+        Translate::Success { data } => {
+            data
                 .translations
                 .iter()
                 .map(|t| t.translated_text.to_owned())
                 .collect::<Vec<_>>()
                 .join(",")
-        }
-        Err(e) => {
-            let response_error = serde_json::from_str::<TranslateResponseError>(&buffer);
-            match response_error {
-                Ok(e) => format!("[{}]: {}", e.error.code, e.error.message),
-                Err(_) => format!("{:?}", e),
-            }
-        }
+
+        },
+        Translate::Error { error } => {
+            format!("[{}]: {}", error.code, error.message)
+        },
     }
 }
 
