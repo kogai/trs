deaf35c81abe9949e38c71401fee8a6283464a47
Shinichi Kogai <kogai0121@gmail.com>
2018-04-23 21:23:10 +0900

    Simplify structure of defnitions

--- src/oxford.rs
+++ src/oxford.rs
@@ -3,12 +3,17 @@ use oxford_dictionary_api_rs::{apis, models};
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
+use std::fmt::{self, Display, Formatter};
 
 header! { (AppId, "app_id") => [String] }
 header! { (AppKey, "app_key") => [String] }
 
 pub fn definitions(query_text: &String) -> String {
   enumlate_examples(call(query_text))
+    .iter()
+    .map(|d| format!("{}", d))
+    .collect::<Vec<String>>()
+    .join("\n\n")
 }
 
 fn call(query_text: &String) -> models::RetrieveEntry {
@@ -35,11 +40,61 @@ fn call(query_text: &String) -> models::RetrieveEntry {
   }
 }
 
-fn enumlate_examples(json: models::RetrieveEntry) -> String {
+type DefinitionAndExample = (Option<String>, Option<String>);
+
+struct Definition {
+  lexical_category: String,
+  definition_and_examples: Vec<DefinitionAndExample>,
+}
+
+impl Definition {
+  fn new(lexical_category: &String, definition_and_examples: Vec<DefinitionAndExample>) -> Self {
+    Definition {
+      lexical_category: lexical_category.to_owned(),
+      definition_and_examples,
+    }
+  }
+}
+
+impl Display for Definition {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    write!(
+      f,
+      "●Lexixal category [{}]\n{}",
+      self.lexical_category,
+      self
+        .definition_and_examples
+        .iter()
+        .map(|d_and_e| match d_and_e {
+          &(Some(ref d), Some(ref e)) => format!(
+            r"
+Definition: {}
+Example: {}",
+            d, e
+          ),
+          &(Some(ref d), None) => format!(
+            r"
+Definition: {}",
+            d
+          ),
+          &(None, Some(ref e)) => format!(
+            r"
+Example: {}",
+            e
+          ),
+          _ => format!(""),
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
+    )
+  }
+}
+
+fn enumlate_examples(json: models::RetrieveEntry) -> Vec<Definition> {
   match json.results() {
     Some(results) => results
       .iter()
-      .map(|result| {
+      .flat_map(|result| {
         result
           .lexical_entries()
           .iter()
@@ -48,45 +103,44 @@ fn enumlate_examples(json: models::RetrieveEntry) -> String {
             let entries = match lexical_entry.entries() {
               Some(entries) => entries
                 .iter()
-                .map(|entry| match entry.senses() {
+                .flat_map(|entry| match entry.senses() {
                   Some(senses) => senses
                     .iter()
                     .map(|sense| match (sense.definitions(), sense.examples()) {
-                      (Some(definitions), Some(examples)) => format!(
-                        "Definition:\n{}\n\nExamples:\n{}",
-                        definitions.join("\n"),
-                        examples
-                          .iter()
-                          .map(|example| example.text().to_owned())
-                          .collect::<Vec<String>>()
-                          .join("\n")
+                      (Some(definitions), Some(examples)) => (
+                        Some(definitions.join("\n")),
+                        Some(
+                          examples
+                            .iter()
+                            .map(|example| example.text().to_owned())
+                            .collect::<Vec<String>>()
+                            .join("\n"),
+                        ),
                       ),
-                      (Some(definitions), _) => format!("Definition: {}", definitions.join("\n\n")),
-                      (_, Some(examples)) => format!(
-                        "Examples: {}",
-                        examples
-                          .iter()
-                          .map(|example| example.text().to_owned())
-                          .collect::<Vec<String>>()
-                          .join("\n\n")
+                      (Some(definitions), _) => (Some(definitions.join("\n")), None),
+                      (_, Some(examples)) => (
+                        None,
+                        Some(
+                          examples
+                            .iter()
+                            .map(|example| example.text().to_owned())
+                            .collect::<Vec<String>>()
+                            .join("\n"),
+                        ),
                       ),
-                      (_, _) => "".to_owned(),
+                      (_, _) => (None, None),
                     })
-                    .collect::<Vec<String>>()
-                    .join("\n\n"),
-                  _ => "".to_owned(),
+                    .collect(),
+                  _ => Vec::new(),
                 })
-                .collect::<Vec<String>>()
-                .join("\n"),
-              _ => "".to_owned(),
+                .collect(),
+              _ => Vec::new(),
             };
-            format!("●Lexixal category [{}]\n\n{}", lexical_category, entries)
+            Definition::new(lexical_category, entries)
           })
-          .collect::<Vec<String>>()
-          .join("\n\n\n")
+          .collect::<Vec<Definition>>()
       })
-      .collect::<Vec<String>>()
-      .join("\n"),
-    None => "".to_owned(),
+      .collect(),
+    None => Vec::new(),
   }
 }
